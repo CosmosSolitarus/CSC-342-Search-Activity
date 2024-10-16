f950d06b950b4c5c4d942e58b0bbd098d566a590
CosmosSolitarus <156846497+CosmosSolitarus@users.noreply.github.com>
2024-10-16 10:49:44 -0400

    First commit
    need to organize files and set up venv

---
+++ search.js
@@ -0,0 +1,11 @@
+import tracks23 from '.top_tracks_2023.js';
+import tracks24 from '.top_tracks_2024.js';
+
+function search() {
+    const input = document.getElementById("searchbar").value;
+
+    console.log(tracks23[0]);
+    console.log(tracks24[3]);
+}
+
+// THIS NEEDS TO BE PYTHON
